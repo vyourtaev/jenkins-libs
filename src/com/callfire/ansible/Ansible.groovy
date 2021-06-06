681f66bd5349c70fcb1b6b9b367a0550df3da1fa
Vladimir Yourtaev <vyourtaev@callfire.com>
2021-06-06 13:30:07 +0300

    .gitignor fefactored

--- src/com/callfire/ansible/Ansible.groovy
+++ src/com/callfire/ansible/Ansible.groovy
@@ -13,7 +13,7 @@ def construct(Map pipelineParams=[:]) {
         subset: 'vault',
         pattern: 'vault'
     ]
-    ansibleEnv = ansibleEnv + [:]
+    ansibleEnv = ansibleEnv + pipelineParams
 
     node {
         checkout([
